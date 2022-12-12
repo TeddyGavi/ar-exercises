e7b9130e71329d2e19cdabf595da840633655af6
Matthew Davis <matc.davis@gmail.com>
2022-12-12 22:51:26 +0000

    working through ex 7 by testing validations one at a time

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts @store2.employees.create(first_name: 'John', hourly_rate: 70).valid?
+puts @store6.employees.create(first_name: "Bidy", last_name: "Son", hourly_rate: 80).valid?
+puts @store4.employees.create(first_name: "Jones", last_name: "Walker", hourly_rate: 20).valid?
+puts @store4.employees.create(first_name: "Jones", last_name: "Walker", hourly_rate: 300).valid?
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  # validates :name, length: {mininum: 3}
+  # validates :annual_revenue, numericality: { only_integer: true }
+
+
 end
