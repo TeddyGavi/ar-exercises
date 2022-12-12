21740e4233f5fed4d80e27a0feca605a092ad21e
Matthew Davis <matc.davis@gmail.com>
2022-12-12 22:58:28 +0000

    add validates inclsuion of with a number range

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,7 +2,8 @@ class Employee < ActiveRecord::Base
   belongs_to :store
   validates :first_name, presence: true 
   validates :last_name, presence: true
-  validates :hourly_rate, numericality: {only_integer: true, greater_than: 40, less_than: 200} 
+  validates :hourly_rate, numericality: { only_integer: true } 
+  validates_inclusion_of :hourly_rate, :in => 40..200
   # validates :store, presence: true, uniqueness: true
   
 end
